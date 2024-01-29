91405c74a2a7ac9013d2848d472c88c4b3bde47f
hazim-j <hazim.jumali@gmail.com>
2024-01-29 19:35:45 +1100

    keep convention

--- build_scripts/nitro.sh
+++ build_scripts/nitro.sh
@@ -1,27 +1,27 @@
 #!/bin/bash
 
+# Check for the presence of the --only-setup flag.
+# This is used in CI to only setup the client directory for other build methods (e.g. docker).
+ONLY_SETUP=0
+for arg in "$@"
+do
+    if [ "$arg" == "--only-setup" ]; then
+        ONLY_SETUP=1
+        break
+    fi
+done
+
 client_path=$(pwd)/nitro/go-ethereum
 src_tracer_path=$(pwd)/tracers/bundler_collector.go.template
 src_arbitrum_extra_path=$(pwd)/tracers/bundler_tracer_arbitrum.go.template
 dest_tracer_path=${client_path}/eth/tracers/native/bundler_collector.go
 dest_arbitrum_extra_path=${client_path}/eth/tracers/native/bundler_tracer_arbitrum.go
 
-# Check for the presence of the --cleanup flag.
-# This is used locally to reset the client directory after docker is done building.
-CLEANUP=0
-for arg in "$@"
-do
-    if [ "$arg" == "--cleanup" ]; then
-        CLEANUP=1
-        break
-    fi
-done
-
 echo "Copy tracers to relevant client directory..."
 cp $src_tracer_path $dest_tracer_path
 cp $src_arbitrum_extra_path $dest_arbitrum_extra_path
 
-if [ $CLEANUP -eq 1 ]; then
+if [ $ONLY_SETUP -eq 0 ]; then
     echo "Clean up client directory..."
     cd $client_path
     git reset --hard
